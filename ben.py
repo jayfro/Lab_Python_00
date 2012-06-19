7473eb32a63ce368bb9d14fb7ac26c23bf9122c8
jayfro <lilclinx@yahoo.co.nz>
2012-06-19 15:37:37 +0000

    {{python assignment code execution }}

---
+++ ben.py
@@ -0,0 +1,33 @@
+print "Benedictus Ashigbui"
+ 
+print 11/2
+
+print 11.0/2
+
+print 3**27
+
+print(134533+6889980-84849)**3
+
+print "The average time per meter is "
+
+print(7.9*10**3)/45
+
+print 'converting to miles'
+
+print 'north'
+
+print(4.7*10**3)
+
+print 4700.0*0.621371
+
+print 'east'
+
+print 9.4*10**3
+
+print 9400.0*0.62137
+
+print "shortest distance"
+
+print pow ((5840.878**2 + 2920.4437**2),0.5)
+6530.30223004676
+
